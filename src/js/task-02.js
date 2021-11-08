0173250f4616cb7d241c5780aecc0282a5d2dbc3
VorobyovaAnna <anyasonn1k@gmail.com>
2021-11-08 22:30:08 +0300

    add task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeList = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add('item');
+    itemEl.textContent = ingredient;
+
+    return itemEl;
+  });
+};
+const listItemsEl = document.querySelector('ul#ingredients');
+
+const items = makeList(ingredients);
+listItemsEl.append(...items);
