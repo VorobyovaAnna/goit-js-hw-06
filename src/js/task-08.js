dcb238314464616505ddb0d469338c704bcbf588
VorobyovaAnna <anyasonn1k@gmail.com>
2021-11-17 10:28:26 +0200

    add task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('form.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (!event.currentTarget.elements.email.value || !event.currentTarget.elements.password.value) {
+    alert('Пожалуйста, заполните поля "Email" и "Password"');
+    }
+    const formData = new FormData(event.currentTarget);
+
+  console.log(formData);
+
+  formData.forEach((value, name) => {
+    console.log('onFormSubmit -> name', name);
+    console.log('onFormSubmit -> value', value);
+  });
+    formEl.reset();
+}
