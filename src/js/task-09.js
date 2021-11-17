dba08b0a80d20832e47dd708e657002adc1a534e
VorobyovaAnna <anyasonn1k@gmail.com>
2021-11-17 11:13:46 +0200

    add task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
+const refs = {
+  button: document.querySelector('button.change-color'),
+  span: document.querySelector('span.color'),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBody = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor();
+};
+
+refs.button.addEventListener('click', changeColorBody);
